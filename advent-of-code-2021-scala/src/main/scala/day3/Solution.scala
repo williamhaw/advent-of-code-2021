b324e9d43011bcac8ebe405338cb9da4d184a5ed
William Haw <me@williamhaw.com>
2021-12-03 14:14:33 +0800

    Add day 3 part 2 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day3/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day3/Solution.scala
@@ -1,23 +1,57 @@
 package day3
 
+import scala.annotation.tailrec
+
 object Solution {
 
   def parse(line: String): List[Char] = line.toCharArray().toList
 
   def getGammaRate(input: List[List[Char]]): Int = {
-      val columns = input.transpose
+    val columns = input.transpose
 
-      val resultList = columns.map(col => if (col.count(_ == '0') > col.count(_ == '1')) '0' else '1')
+    val resultList = columns.map(col => if (col.count(_ == '0') > col.count(_ == '1')) '0' else '1')
 
-      Integer.parseInt(resultList.mkString(""), 2)
+    Integer.parseInt(resultList.mkString(""), 2)
   }
 
   def getEpsilonRate(input: List[List[Char]]): Int = {
-      val columns = input.transpose
+    val columns = input.transpose
 
-      val resultList = columns.map(col => if (col.count(_ == '0') < col.count(_ == '1')) '0' else '1')
+    val resultList = columns.map(col => if (col.count(_ == '0') < col.count(_ == '1')) '0' else '1')
 
-      Integer.parseInt(resultList.mkString(""), 2)
+    Integer.parseInt(resultList.mkString(""), 2)
   }
 
+  def getOxygenGeneratorRating(input: List[List[Char]]): Int = {
+
+    val disc = (c: List[Char]) => if (c.count(_ == '0') > c.count(_ == '1')) '0' else '1'
+
+    Integer.parseInt(helper(input, 0, disc).mkString(""), 2)
+  }
+
+  def getCO2ScrubberRating(input: List[List[Char]]): Int = {
+    val disc = (c: List[Char]) => if (c.count(_ == '0') <= c.count(_ == '1')) '0' else '1'
+
+    Integer.parseInt(helper(input, 0, disc).mkString(""), 2)
+  }
+
+  @tailrec
+  def helper(
+      input: List[List[Char]],
+      index: Int,
+      discriminator: (List[Char]) => Char
+  ): List[Char] = {
+    if (input.size == 1)
+      input.head
+    else {
+      val columns = input.transpose
+
+      val currentColumn = columns(index)
+
+      val currentBit = discriminator(currentColumn)
+
+      val newInput: List[List[Char]] = input.filter(l => l(index) == currentBit)
+      helper(newInput, index + 1, discriminator)
+    }
+  }
 }
--- advent-of-code-2021-scala/src/test/scala/day3/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day3/SolutionSpec.scala
@@ -7,25 +7,45 @@ import day3.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day3.SolutionSpec")(
-    test("getGammaRate should give 22 given input") {
+    test("getGammaRate should give 22 given test input") {
       for {
         input <- ZFileReader.readLines("day3-test-input.txt")(parse)
       } yield assert(getGammaRate(input))(equalTo(22))
     },
-    test("getEpsilonRate should give 9 given input") {
+    test("getGammaRate should give correct rate for real input") {
+      for {
+        input <- ZFileReader.readLines("day-3-input-william.txt")(parse)
+      } yield assert(getGammaRate(input))(equalTo(3349))
+    },
+    test("getEpsilonRate should give 9 given test input") {
       for {
         input <- ZFileReader.readLines("day3-test-input.txt")(parse)
       } yield assert(getEpsilonRate(input))(equalTo(9))
     },
-    test("getGammaRate should give correct rate for real input"){
-        for {
-        input <- ZFileReader.readLines("day-3-input-william.txt")(parse)
-      } yield assert(getGammaRate(input))(equalTo(3349))
-    },
-    test("getEpsilonRate should give correct rate for real input"){
-        for {
+    test("getEpsilonRate should give correct rate for real input") {
+      for {
         input <- ZFileReader.readLines("day-3-input-william.txt")(parse)
       } yield assert(getEpsilonRate(input))(equalTo(746))
+    },
+    test("getOxygenGeneratorRating should give 23 given test input") {
+      for {
+        input <- ZFileReader.readLines("day3-test-input.txt")(parse)
+      } yield assert(getOxygenGeneratorRating(input))(equalTo(23))
+    },
+    test("getOxygenGeneratorRating should give correct rate for real input") {
+      for {
+        input <- ZFileReader.readLines("day-3-input-william.txt")(parse)
+      } yield assert(getOxygenGeneratorRating(input))(equalTo(3921))
+    },
+    test("getCO2ScrubberRating should give 10 given test input") {
+      for {
+        input <- ZFileReader.readLines("day3-test-input.txt")(parse)
+      } yield assert(getCO2ScrubberRating(input))(equalTo(10))
+    },
+    test("getCO2ScrubberRating should give correct rate for real input") {
+      for {
+        input <- ZFileReader.readLines("day-3-input-william.txt")(parse)
+      } yield assert(getCO2ScrubberRating(input))(equalTo(836))
     }
   )
 }

0a04cabab283df6e0d2bcb199961213afa939618
William Haw <me@williamhaw.com>
2021-12-05 13:58:44 +0800

    Add day 5 part 1 solution and test

--- advent-of-code-2021-scala/src/main/scala/day5/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day5/Solution.scala
@@ -1,5 +1,28 @@
 package day5
 
 object Solution {
-  
+
+  case class Point(x: Int, y: Int)
+  case class Segment(p1: Point, p2: Point)
+
+  def parseSegment(line: String): Segment = {
+    val points = line.split(" -> ").map(p => Point(p.split(",")(0).toInt, p.split(",")(1).toInt))
+    Segment(points(0), points(1))
+  }
+
+  def filterOnlyHorizontalAndVerticalSegments(input: List[Segment]): List[Segment] =
+    input.filter(s => s.p1.x == s.p2.x || s.p1.y == s.p2.y)
+
+  def getSumOfHorizontalAndVerticalIntersections(input: List[Segment]): Int = {
+    val allPoints = input.map { segment =>
+      for {
+        x <- (segment.p1.x min segment.p2.x) to (segment.p1.x max segment.p2.x)
+        y <- (segment.p1.y min segment.p2.y) to (segment.p1.y max segment.p2.y)
+      } yield {
+        Point(x, y)
+      }
+    }
+    allPoints.flatten.groupBy(identity).map(_._2.size).filter(_ >= 2).size
+  }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day5/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day5/SolutionSpec.scala
@@ -7,6 +7,17 @@ import day5.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day5.SolutionSpec")(
-    test("")(assert(true)(equalTo(true)))
+    test("getSumOfHorizontalAndVerticalIntersections should return correct sum of intersections from test input")(
+      for{
+          segments <- ZFileReader.readLines("day5-test-input.txt")(parseSegment)
+          filteredSegments = filterOnlyHorizontalAndVerticalSegments(segments)
+      } yield assert(getSumOfHorizontalAndVerticalIntersections(filteredSegments))(equalTo(5))
+    ),
+    test("getSumOfHorizontalAndVerticalIntersections should return correct sum of intersections from real input")(
+      for{
+          segments <- ZFileReader.readLines("day-5-input-william.txt")(parseSegment)
+          filteredSegments = filterOnlyHorizontalAndVerticalSegments(segments)
+      } yield assert(getSumOfHorizontalAndVerticalIntersections(filteredSegments))(equalTo(5608))
+    )
   )
 }

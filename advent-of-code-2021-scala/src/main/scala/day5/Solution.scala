add8deecbc8791d4d08266a6bfe9dbb2b4b582fb
William Haw <me@williamhaw.com>
2021-12-05 16:15:42 +0800

    Add day 5 part 2 solution and test

--- advent-of-code-2021-scala/src/main/scala/day5/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day5/Solution.scala
@@ -13,8 +13,11 @@ object Solution {
   def filterOnlyHorizontalAndVerticalSegments(input: List[Segment]): List[Segment] =
     input.filter(s => s.p1.x == s.p2.x || s.p1.y == s.p2.y)
 
-  def getSumOfHorizontalAndVerticalIntersections(input: List[Segment]): Int = {
-    val allPoints = input.map { segment =>
+  def filterDiagonalSegments(input: List[Segment]): List[Segment] =
+    input.filter(s => (s.p1.x - s.p2.x).abs == (s.p1.y - s.p2.y).abs)
+
+  def getHorizontalAndVerticalPoints(input: List[Segment]): List[Point] = {
+    val points = filterOnlyHorizontalAndVerticalSegments(input).map { segment =>
       for {
         x <- (segment.p1.x min segment.p2.x) to (segment.p1.x max segment.p2.x)
         y <- (segment.p1.y min segment.p2.y) to (segment.p1.y max segment.p2.y)
@@ -22,7 +25,32 @@ object Solution {
         Point(x, y)
       }
     }
-    allPoints.flatten.groupBy(identity).map(_._2.size).filter(_ >= 2).size
+    points.flatten
+  }
+
+  def getSumOfHorizontalAndVerticalIntersections(input: List[Segment]): Int = {
+    val points = getHorizontalAndVerticalPoints(input)
+    points.groupBy(identity).map(_._2.size).filter(_ >= 2).size
+  }
+
+  def getDiagonalPoints(input: List[Segment]): List[Point] = {
+    val points = filterDiagonalSegments(input).map { segment =>
+      for {
+        x <- (segment.p1.x min segment.p2.x) to (segment.p1.x max segment.p2.x)
+        y <- (segment.p1.y min segment.p2.y) to (segment.p1.y max segment.p2.y)
+      } yield {
+        if ((x - segment.p1.x).abs == (y - segment.p1.y).abs) // gradient can only be 1 or -1
+          Some(Point(x, y))
+        else
+          None
+      }
+    }
+    points.flatten.flatten
+  }
+
+  def getSumOfHorizontalVerticalDiagonalIntersections(input: List[Segment]): Int = {
+    val points = getHorizontalAndVerticalPoints(input) :++ getDiagonalPoints(input)
+    points.groupBy(identity).map(_._2.size).filter(_ >= 2).size
   }
 
 }
--- advent-of-code-2021-scala/src/test/scala/day5/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day5/SolutionSpec.scala
@@ -8,16 +8,42 @@ import day5.Solution._
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day5.SolutionSpec")(
     test("getSumOfHorizontalAndVerticalIntersections should return correct sum of intersections from test input")(
-      for{
-          segments <- ZFileReader.readLines("day5-test-input.txt")(parseSegment)
-          filteredSegments = filterOnlyHorizontalAndVerticalSegments(segments)
-      } yield assert(getSumOfHorizontalAndVerticalIntersections(filteredSegments))(equalTo(5))
+      for {
+        segments <- ZFileReader.readLines("day5-test-input.txt")(parseSegment)
+      } yield assert(getSumOfHorizontalAndVerticalIntersections(segments))(equalTo(5))
     ),
     test("getSumOfHorizontalAndVerticalIntersections should return correct sum of intersections from real input")(
-      for{
-          segments <- ZFileReader.readLines("day-5-input-william.txt")(parseSegment)
-          filteredSegments = filterOnlyHorizontalAndVerticalSegments(segments)
-      } yield assert(getSumOfHorizontalAndVerticalIntersections(filteredSegments))(equalTo(5608))
+      for {
+        segments <- ZFileReader.readLines("day-5-input-william.txt")(parseSegment)
+      } yield assert(getSumOfHorizontalAndVerticalIntersections(segments))(equalTo(5608))
+    ),
+    test("getDiagonalPoints") {
+      val segmentsPlusPlus   = List(Segment(Point(0, 0), Point(3, 3)))
+      val segmentsMinusPlus  = List(Segment(Point(3, 0), Point(0, 3)))
+      val segmentsPlusMinus  = List(Segment(Point(0, 3), Point(3, 0)))
+      val segmentsMinusMinus = List(Segment(Point(3, 3), Point(0, 0)))
+      assert(getDiagonalPoints(segmentsPlusPlus))(
+        equalTo(List(Point(0, 0), Point(1, 1), Point(2, 2), Point(3, 3)))
+      ) &&
+      assert(getDiagonalPoints(segmentsMinusPlus))(
+        equalTo(List(Point(0, 3), Point(1, 2), Point(2, 1), Point(3, 0)))
+      ) &&
+      assert(getDiagonalPoints(segmentsPlusMinus))(
+        equalTo(List(Point(0, 3), Point(1, 2), Point(2, 1), Point(3, 0)))
+      ) &&
+      assert(getDiagonalPoints(segmentsMinusMinus))(
+        equalTo(List(Point(0, 0), Point(1, 1), Point(2, 2), Point(3, 3)))
+      )
+    },
+    test("getSumOfHorizontalVerticalDiagonalIntersections should return correct sum of intersections from test input")(
+      for {
+        segments <- ZFileReader.readLines("day5-test-input.txt")(parseSegment)
+      } yield assert(getSumOfHorizontalVerticalDiagonalIntersections(segments))(equalTo(12))
+    ),
+    test("getSumOfHorizontalVerticalDiagonalIntersections should return correct sum of intersections from real input")(
+      for {
+        segments <- ZFileReader.readLines("day-5-input-william.txt")(parseSegment)
+      } yield assert(getSumOfHorizontalVerticalDiagonalIntersections(segments))(equalTo(20299))
     )
   )
 }

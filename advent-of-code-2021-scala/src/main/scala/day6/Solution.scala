8a4fd2b1696d505d88c02fd2246ad9b16ad4ffe0
William Haw <me@williamhaw.com>
2021-12-06 15:36:51 +0800

    Add day 6 solutions

--- advent-of-code-2021-scala/src/main/scala/day6/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day6/Solution.scala
@@ -1,5 +1,40 @@
 package day6
 
+import scala.annotation.tailrec
+
 object Solution {
-  
+
+  /** @param input
+    * @return
+    *   Map where key is the day and value is the number of fishes with timer values corresponding to that day.
+    */
+  def parse(input: String): Map[Int, Long] = {
+    val empty = Map(0 -> 0L, 1 -> 0L, 2 -> 0L, 3 -> 0L, 4 -> 0L, 5 -> 0L, 6 -> 0L, 7 -> 0L, 8 -> 0L)
+    val values = input
+      .strip()
+      .split(",")
+      .map(_.toInt)
+      .groupBy(identity)
+      .map { case (timer, array) => (timer, array.size.toLong) }
+
+    (empty.toSeq ++ values.toSeq).groupBy(_._1).mapValues(_.map(_._2).sum).toMap
+  }
+
+  def simulateDay(timers: Map[Int, Long]): Map[Int, Long] =
+    timers.keys.flatMap { t =>
+      t match {
+        case 0                 => List((6, timers(0) + timers(7)), (8, timers(0)))
+        case tNow if tNow == 7 => List()
+        case tNow              => List((tNow - 1, timers(tNow)))
+      }
+    }.toMap
+
+  @tailrec
+  def runSimForNDays(days: Int, timers: Map[Int, Long]): Map[Int, Long] =
+    if (days == 0)
+      timers
+    else
+      runSimForNDays(days - 1, simulateDay(timers))
+
+  def getTotalFishCount(timers: Map[Int, Long]): Long = timers.foldLeft(0L)(_ + _._2)
 }
--- advent-of-code-2021-scala/src/test/scala/day6/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day6/SolutionSpec.scala
@@ -5,10 +5,34 @@ import Assertion._
 import common.ZFileReader
 import day6.Solution._
 
-object SolutionSpec extends DefaultRunnableSpec{
+object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day6.SolutionSpec")(
-      test(""){
-          assertTrue(true)
-      }
+    test("simulateDay should return next day's timers for test input") {
+      for {
+        timers <- ZFileReader.readLines("day6-test-input.txt")(parse).head
+      } yield assert(simulateDay(timers))(
+        equalTo(Map(0 -> 1L, 1 -> 1L, 2 -> 2L, 3 -> 1L, 4 -> 0L, 5 -> 0L, 6 -> 0L, 7 -> 0L, 8 -> 0L))
+      )
+    },
+    test("runSimForNDays should return the correct number of fish on the 18th day for test input") {
+      for {
+        timers <- ZFileReader.readLines("day6-test-input.txt")(parse).head
+      } yield assert(getTotalFishCount(runSimForNDays(18, timers)))(equalTo(26L))
+    },
+    test("runSimForNDays should return the correct number of fish on the 256th day for test input") {
+      for {
+        timers <- ZFileReader.readLines("day6-test-input.txt")(parse).head
+      } yield assert(getTotalFishCount(runSimForNDays(256, timers)))(equalTo(26984457539L))
+    },
+    test("runSimForNDays should return the correct number of fish on the 80th day for real input") {
+      for {
+        timers <- ZFileReader.readLines("day-6-input-william.txt")(parse).head
+      } yield assert(getTotalFishCount(runSimForNDays(80, timers)))(equalTo(393019L))
+    },
+    test("runSimForNDays should return the correct number of fish on the 256th day for real input") {
+      for {
+        timers <- ZFileReader.readLines("day-6-input-william.txt")(parse).head
+      } yield assert(getTotalFishCount(runSimForNDays(256, timers)))(equalTo(1757714216975L))
+    }
   )
 }

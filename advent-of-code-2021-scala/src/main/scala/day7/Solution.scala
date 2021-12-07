8d7678c8a0cc7d5af800b0a22f0e015fda8b5fa0
William Haw <me@williamhaw.com>
2021-12-07 17:03:11 +0800

    Add day 7 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day7/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day7/Solution.scala
@@ -1,5 +1,22 @@
 package day7
 
 object Solution {
-  
+
+  def parse(line: String): List[Int] = line.split(",").map(_.toInt).toList
+
+  def getLowestTotalFuelCost(crabPositions: List[Int]): Int = {
+    val maxPosition = crabPositions.max
+    val positions   = Vector.tabulate(maxPosition + 1)(i => crabPositions.count(_ == i))
+
+    val costs = for {
+      costIdx <- 0 until positions.size
+    } yield {
+      for {
+        positionIdx <- 0 until positions.size
+      } yield positions(positionIdx) * (positionIdx - costIdx).abs
+    }.sum
+
+    costs.min
+  }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day7/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day7/SolutionSpec.scala
@@ -6,9 +6,15 @@ import common.ZFileReader
 import day7.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
-  def spec = suite("")(
-    test("") {
-      assertTrue(true)
+  def spec = suite("day7.SolutionSpec")(
+    test("getLowestTotalFuelCost should get the lowest cost for the test input") {
+      val testInput = "16,1,2,0,4,2,7,1,2,14"
+      assert(getLowestTotalFuelCost(parse(testInput)))(equalTo(37))
+    },
+    test("getLowestTotalFuelCost should get the lowest cost for the real input") {
+      for {
+        input <- ZFileReader.readLines("day-7-input-william.txt")(parse).head
+      } yield assert(getLowestTotalFuelCost(input))(equalTo(357353))
     }
   )
 }

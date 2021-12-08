e7fa4d4d77a2aea70306eea21f50b4aa45021966
William Haw <me@williamhaw.com>
2021-12-08 14:50:17 +0800

    Add day 8 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day8/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day8/Solution.scala
@@ -1,5 +1,22 @@
 package day8
 
 object Solution {
-  
+
+  case class SignalOutput(signals: Seq[String], outputDigits: Seq[String])
+
+  def parse(line: String): SignalOutput =
+    line match {
+      case s"$signals | $outputs" => SignalOutput(signals.split(" "), outputs.split(" "))
+    }
+
+  val segmentCount = Set(
+    2, // digit '1'
+    4, // digit '4'
+    3, // digit '7'
+    7  // digit '8'
+  )
+
+  def getCountOfOneFourSevenEights(input: List[SignalOutput]): Int =
+    input.foldLeft(0) { case (count, so) => count + so.outputDigits.count(s => segmentCount.contains(s.length())) }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day8/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day8/SolutionSpec.scala
@@ -7,8 +7,15 @@ import day8.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day8.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("getCountOfOneFourSevenEights should give correct count for test input") {
+      for{
+          signalOutputs <- ZFileReader.readLines("day8-test-input.txt")(parse)
+      } yield assert(getCountOfOneFourSevenEights(signalOutputs))(equalTo(26))
+    },
+    test("getCountOfOneFourSevenEights should give correct count for real input") {
+      for{
+          signalOutputs <- ZFileReader.readLines("day-8-input-william.txt")(parse)
+      } yield assert(getCountOfOneFourSevenEights(signalOutputs))(equalTo(255))
     }
   )
 }

54a453f212711c38c8cc9ff7b1085d8bb3c1f8a2
William Haw <me@williamhaw.com>
2021-12-09 00:43:04 +0800

    Add day 8 part 2 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day8/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day8/Solution.scala
@@ -19,4 +19,37 @@ object Solution {
   def getCountOfOneFourSevenEights(input: List[SignalOutput]): Int =
     input.foldLeft(0) { case (count, so) => count + so.outputDigits.count(s => segmentCount.contains(s.length())) }
 
+  def getDigitFromSignal(signals: Seq[String]): Map[Set[Char], Int] = {
+
+    val `1` = signals.find(_.length() == 2).get.toSet
+    val `4` = signals.find(_.length() == 4).get.toSet
+
+    signals.map { signal =>
+      signal match {
+        case s if s.length() == 2 => (s.toSet, 1)
+        case s if s.length() == 4 => (s.toSet, 4)
+        case s if s.length() == 3 => (s.toSet, 7)
+        case s if s.length() == 7 => (s.toSet, 8)
+        case s if s.length() == 6 =>
+          s.toSet match {
+            case chars if chars.intersect(`1`).size == 1 => (chars, 6)
+            case chars if chars.intersect(`4`).size == 4 => (chars, 9)
+            case chars                                   => (chars, 0)
+          }
+        case s if s.length() == 5 =>
+          s.toSet match {
+            case chars if chars.intersect(`1`).size == 2 => (chars, 3)
+            case chars if chars.intersect(`4`).size == 3 => (chars, 5)
+            case chars                                   => (chars, 2)
+          }
+      }
+    }.toMap
+  }
+
+  def getOutputFromSignal(outputDigits: Seq[String], signalMap: Map[Set[Char], Int]): Int =
+    outputDigits.map(o => signalMap(o.toSet)).foldLeft(0)((acc, ele) => acc * 10 + ele)
+
+  def getSumOfOutputs(signalOutputs: List[SignalOutput]): Int =
+    signalOutputs.map(so => getOutputFromSignal(so.outputDigits, getDigitFromSignal(so.signals))).sum
+
 }
--- advent-of-code-2021-scala/src/test/scala/day8/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day8/SolutionSpec.scala
@@ -8,14 +8,43 @@ import day8.Solution._
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day8.SolutionSpec")(
     test("getCountOfOneFourSevenEights should give correct count for test input") {
-      for{
-          signalOutputs <- ZFileReader.readLines("day8-test-input.txt")(parse)
+      for {
+        signalOutputs <- ZFileReader.readLines("day8-test-input.txt")(parse)
       } yield assert(getCountOfOneFourSevenEights(signalOutputs))(equalTo(26))
     },
     test("getCountOfOneFourSevenEights should give correct count for real input") {
-      for{
-          signalOutputs <- ZFileReader.readLines("day-8-input-william.txt")(parse)
+      for {
+        signalOutputs <- ZFileReader.readLines("day-8-input-william.txt")(parse)
       } yield assert(getCountOfOneFourSevenEights(signalOutputs))(equalTo(255))
+    },
+    test("getDigitFromSignal should decode signals for test input") {
+      val input = parse("acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf")
+      assert(getDigitFromSignal(input.signals))(
+        equalTo(
+          Map(
+            "acedgfb".toSet -> 8,
+            "cdfbe".toSet   -> 5,
+            "gcdfa".toSet   -> 2,
+            "fbcad".toSet   -> 3,
+            "dab".toSet     -> 7,
+            "cefabd".toSet  -> 9,
+            "cdfgeb".toSet  -> 6,
+            "eafb".toSet    -> 4,
+            "cagedb".toSet  -> 0,
+            "ab".toSet      -> 1
+          )
+        )
+      ) && assert(getOutputFromSignal(input.outputDigits, getDigitFromSignal(input.signals)))(equalTo(5353))
+    },
+    test("getSumOfOutputs should get correct sum for test input"){
+        for {
+        signalOutputs <- ZFileReader.readLines("day8-test-input.txt")(parse)
+      } yield assert(getSumOfOutputs(signalOutputs))(equalTo(61229))
+    },
+    test("getSumOfOutputs should get correct sum for real input"){
+        for {
+        signalOutputs <- ZFileReader.readLines("day-8-input-william.txt")(parse)
+      } yield assert(getSumOfOutputs(signalOutputs))(equalTo(982158))
     }
   )
 }

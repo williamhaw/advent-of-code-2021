1e9be361bf33e4b6c371d7597cc2689f339f338b
William Haw <me@williamhaw.com>
2021-12-09 16:18:50 +0800

    Refactor day 9 part 1 solution and tests to find lowest points

--- advent-of-code-2021-scala/src/main/scala/day9/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day9/Solution.scala
@@ -11,23 +11,22 @@ object Solution {
       Seq(Seq.fill(finalRowSize)(Int.MaxValue))
   }
 
-  def getRiskLevel(input: Seq[Seq[Int]]): Int = {
-    val levels = for {
+  def getRiskLevel(input: Seq[Seq[Int]]): Int =
+    getLowestPoints(input).map(p => input(p._1)(p._2) + 1).sum
+
+  def getLowestPoints(input: Seq[Seq[Int]]): Seq[(Int, Int)] = (
+    for {
       r <- 1 until input.size - 1
       c <- 1 until input(0).size - 1
-    } yield {
-      val currentHeight = input(r)(c)
+    } yield
       if (
-        input(r - 1)(c) > currentHeight &&
-        input(r + 1)(c) > currentHeight &&
-        input(r)(c - 1) > currentHeight &&
-        input(r)(c + 1) > currentHeight
+        input(r - 1)(c) > input(r)(c) &&
+        input(r + 1)(c) > input(r)(c) &&
+        input(r)(c - 1) > input(r)(c) &&
+        input(r)(c + 1) > input(r)(c)
       )
-        currentHeight + 1
+        Seq((r, c))
       else
-        0
-    }
-    println(levels.filter(_ != 0))
-    levels.sum
-  }
+        Seq.empty
+  ).flatten
 }
--- advent-of-code-2021-scala/src/test/scala/day9/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day9/SolutionSpec.scala
@@ -8,14 +8,28 @@ import day9.Solution._
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day9.SolutionSpec")(
     test("getRiskLevel should return correct output for test input") {
-        for {
-          input <- ZFileReader.readLines("day9-test-input.txt")(parse)
-        } yield assert(getRiskLevel(processInput(input)))(equalTo(15))
+      for {
+        input <- ZFileReader.readLines("day9-test-input.txt")(parse)
+      } yield assert(getRiskLevel(processInput(input)))(equalTo(15))
     },
     test("getRiskLevel should return correct output for real input") {
-        for {
-          input <- ZFileReader.readLines("day-9-input-william.txt")(parse)
-        } yield assert(getRiskLevel(processInput(input)))(equalTo(15))
+      for {
+        input <- ZFileReader.readLines("day-9-input-william.txt")(parse)
+      } yield assert(getRiskLevel(processInput(input)))(equalTo(577))
+    },
+    test("getLowestPoints should return lowest points for test input") {
+      for {
+        input <- ZFileReader.readLines("day9-test-input.txt")(parse)
+      } yield assert(getLowestPoints(processInput(input)))(
+        equalTo(
+          Seq(
+            (1, 2),
+            (1, 10),
+            (3, 3),
+            (5, 7)
+          )
+        )
+      )
     }
   )
 }

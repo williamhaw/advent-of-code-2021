6b74695a227a5b3c467d0374da220c593dadbf1f
William Haw <me@williamhaw.com>
2021-12-09 13:58:04 +0800

    Add day 9 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day9/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day9/Solution.scala
@@ -1,5 +1,33 @@
 package day9
 
 object Solution {
-  
+
+  def parse(line: String): Seq[Int] = line.map(_.toInt - 48)
+
+  def processInput(rawInput: Seq[Seq[Int]]): Seq[Seq[Int]] = {
+    val finalRowSize = rawInput(0).size + 2
+    Seq(Seq.fill(finalRowSize)(Int.MaxValue)) :++
+      rawInput.map(r => Int.MaxValue +: r :+ Int.MaxValue) :++
+      Seq(Seq.fill(finalRowSize)(Int.MaxValue))
+  }
+
+  def getRiskLevel(input: Seq[Seq[Int]]): Int = {
+    val levels = for {
+      r <- 1 until input.size - 1
+      c <- 1 until input(0).size - 1
+    } yield {
+      val currentHeight = input(r)(c)
+      if (
+        input(r - 1)(c) > currentHeight &&
+        input(r + 1)(c) > currentHeight &&
+        input(r)(c - 1) > currentHeight &&
+        input(r)(c + 1) > currentHeight
+      )
+        currentHeight + 1
+      else
+        0
+    }
+    println(levels.filter(_ != 0))
+    levels.sum
+  }
 }
--- advent-of-code-2021-scala/src/test/scala/day9/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day9/SolutionSpec.scala
@@ -6,9 +6,16 @@ import common.ZFileReader
 import day9.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
-  def spec = suite("")(
-      test(""){
-          assertTrue(true)
-      }
+  def spec = suite("day9.SolutionSpec")(
+    test("getRiskLevel should return correct output for test input") {
+        for {
+          input <- ZFileReader.readLines("day9-test-input.txt")(parse)
+        } yield assert(getRiskLevel(processInput(input)))(equalTo(15))
+    },
+    test("getRiskLevel should return correct output for real input") {
+        for {
+          input <- ZFileReader.readLines("day-9-input-william.txt")(parse)
+        } yield assert(getRiskLevel(processInput(input)))(equalTo(15))
+    }
   )
 }

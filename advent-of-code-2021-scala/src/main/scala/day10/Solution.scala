4eebea029e0d73b8c3c87e9fa348c0ad7160057d
William Haw <me@williamhaw.com>
2021-12-10 17:25:19 +0800

    Add day 10 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day10/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day10/Solution.scala
@@ -1,7 +1,44 @@
 package day10
 
+import scala.annotation.tailrec
+
 object Solution {
 
   val parse: String => String = (line: String) => line
+
+  val points = Map(
+    ')' -> 3,
+    ']' -> 57,
+    '}' -> 1197,
+    '>' -> 25137
+  )
+
+  val openBrackets = Set('(', '[', '{', '<')
+  val correspondingBrackets = Map(
+    ')' -> '(',
+    ']' -> '[',
+    '}' -> '{',
+    '>' -> '<'
+  )
+
+  def getErrorCharacter(line: String): Option[Char] = {
+
+    @tailrec
+    def helper(remaining: String, stack: Seq[Char]): Option[Char] = {
+      if (remaining.isEmpty)
+        None
+      else if (openBrackets.contains(remaining.head))
+        helper(remaining.tail, stack.prepended(remaining.head))
+      else if (correspondingBrackets(remaining.head) == stack.head)
+        helper(remaining.tail, stack.tail)
+      else
+        Some(remaining.head)
+    }
+    val ret = helper(line, List())
+    ret
+  }
+
+  def getErrorScore(input: Seq[String]): Int =
+    input.map(getErrorCharacter).map(_.fold(0)(points)).sum
 
 }
--- advent-of-code-2021-scala/src/test/scala/day10/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day10/SolutionSpec.scala
@@ -7,8 +7,22 @@ import day10.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day10.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("getErrorCharacter returns correct error for line") {
+      assert(getErrorCharacter("{([(<{}[<>[]}>{[]{[(<()>"))(equalTo(Some('}'))) &&
+      assert(getErrorCharacter("[[<[([]))<([[{}[[()]]]"))(equalTo(Some(')'))) &&
+      assert(getErrorCharacter("[{[{({}]{}}([{[{{{}}([]"))(equalTo(Some(']'))) &&
+      assert(getErrorCharacter("[<(<(<(<{}))><([]([]()"))(equalTo(Some(')'))) &&
+      assert(getErrorCharacter("<{([([[(<>()){}]>(<<{{"))(equalTo(Some('>')))
+    },
+    test("getErrorScore should get correct score from test input") {
+      for {
+        input <- ZFileReader.readLines("day10-test-input.txt")(parse)
+      } yield assert(getErrorScore(input))(equalTo(26397))
+    },
+    test("getErrorScore should get correct score from real input") {
+      for {
+        input <- ZFileReader.readLines("day-10-input-william.txt")(parse)
+      } yield assert(getErrorScore(input))(equalTo(268845))
     }
   )
 }

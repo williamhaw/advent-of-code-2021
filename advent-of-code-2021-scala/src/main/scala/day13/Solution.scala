5b28124726833f6110a0b26a0cd1e15bbed4acb7
William Haw <me@williamhaw.com>
2021-12-13 13:48:43 +0800

    Add day 13 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day13/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day13/Solution.scala
@@ -1,5 +1,24 @@
 package day13
 
 object Solution {
-  
+
+  case class Point(x: Int, y: Int)
+
+  sealed trait Dimension
+  case object X extends Dimension
+  case object Y extends Dimension
+
+  case class Fold(dimension: Dimension, value: Int)
+
+  def parseCoords(line: String): Point = line match { case s"$x,$y" => Point(x.toInt, y.toInt) }
+
+  def parseFolds(line: String): Fold = line match { case s"fold along $d=$v" => Fold(if (d == "x") X else Y, v.toInt) }
+
+  def performFold(points: Seq[Point], fold: Fold): Seq[Point] = points.map { p =>
+    fold match {
+      case Fold(X, v) => if (p.x > v) Point(v - (p.x - v), p.y) else p
+      case Fold(Y, v) => if (p.y > v) Point(p.x, v - (p.y - v)) else p
+    }
+  }.distinct
+
 }
--- advent-of-code-2021-scala/src/test/scala/day13/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day13/SolutionSpec.scala
@@ -3,12 +3,21 @@ package day13
 import zio.test._
 import Assertion._
 import common.ZFileReader
-import day11.Solution._
+import day13.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day13.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("performFold should return points left after one fold on test input") {
+      for {
+        points <- ZFileReader.readLines("day13-test-input-coords.txt")(parseCoords)
+        folds  <- ZFileReader.readLines("day13-test-input-folds.txt")(parseFolds)
+      } yield assert(performFold(points, folds.head).size)(equalTo(17))
+    },
+    test("performFold should return points left after one fold on real input") {
+      for {
+        points <- ZFileReader.readLines("day-13-input-coords-william.txt")(parseCoords)
+        folds  <- ZFileReader.readLines("day-13-input-folds-william.txt")(parseFolds)
+      } yield assert(performFold(points, folds.head).size)(equalTo(602))
     }
   )
 }

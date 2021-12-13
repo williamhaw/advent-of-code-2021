a4f70d5a580f646bbd24e74590a1c9decc260c26
William Haw <me@williamhaw.com>
2021-12-13 10:47:09 +0800

    Add day 12 part 1 solution and test

--- advent-of-code-2021-scala/src/main/scala/day12/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day12/Solution.scala
@@ -1,5 +1,31 @@
 package day12
 
 object Solution {
-  
+
+  def parse(line: String): (String, String) = line match {
+    case s"$a-$b" => (a, b)
+  }
+
+  def getAdjacencyList(input: Seq[(String, String)]): Map[String, Seq[String]] =
+    (input ++ input.map(e => (e._2, e._1))).groupBy(_._1).view.mapValues(_.map(_._2)).toMap
+
+  def getPaths(input: Map[String, Seq[String]]): Seq[Seq[String]] = {
+
+    def helper(currentNode: String, visited: Set[String], path: Seq[String]): Seq[Seq[String]] = {
+      if (visited.contains(currentNode))
+        Seq()
+      else if (currentNode == "end")
+        Seq(path :+ "end")
+      else {
+        val currentVisited = if ("[a-z]+".r.matches(currentNode)) visited + currentNode else visited
+        input(currentNode).foldLeft(Seq(Seq[String]())) { case (paths, next) =>
+          paths ++ helper(next, currentVisited, path :+ currentNode)
+        }
+      }
+    }
+
+    helper("start", Set.empty, Seq.empty).filterNot(_.isEmpty)
+
+  }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day12/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day12/SolutionSpec.scala
@@ -7,8 +7,35 @@ import day12.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day12.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("getAdjacencyList should return the adjacency list for test input") {
+      for {
+        input <- ZFileReader.readLines("day12-test-input-small.txt")(parse)
+      } yield assert(getAdjacencyList(input))(
+        equalTo(
+          Map(
+            "A"     -> List("c", "b", "end", "start"),
+            "b"     -> List("d", "end", "start", "A"),
+            "end"   -> List("A", "b"),
+            "c"     -> List("A"),
+            "start" -> List("A", "b"),
+            "d"     -> List("b")
+          )
+        )
+      )
+    },
+    test("getPaths should return the correct number of paths for test input") {
+      for {
+        smallInput  <- ZFileReader.readLines("day12-test-input-small.txt")(parse)
+        mediumInput <- ZFileReader.readLines("day12-test-input-medium.txt")(parse)
+        largeInput  <- ZFileReader.readLines("day12-test-input-large.txt")(parse)
+      } yield assert(getPaths(getAdjacencyList(smallInput)).size)(equalTo(10)) &&
+        assert(getPaths(getAdjacencyList(mediumInput)).size)(equalTo(19)) &&
+        assert(getPaths(getAdjacencyList(largeInput)).size)(equalTo(226))
+    },
+    test("getPaths should return the correct number of paths for real input") {
+      for {
+        input <- ZFileReader.readLines("day-12-input-william.txt")(parse)
+      } yield assert(getPaths(getAdjacencyList(input)).size)(equalTo(3495))
     }
   )
 }

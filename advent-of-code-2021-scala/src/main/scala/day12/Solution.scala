34de438f21d246d5acf50d0ab0acb0253c33e15e
William Haw <me@williamhaw.com>
2021-12-13 12:39:44 +0800

    Add day 12 part 2 solution and test

--- advent-of-code-2021-scala/src/main/scala/day12/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day12/Solution.scala
@@ -6,26 +6,51 @@ object Solution {
     case s"$a-$b" => (a, b)
   }
 
+  val smallCavePattern                   = "[a-z]+".r
+  def isSmallCave(cave: String): Boolean = smallCavePattern.matches(cave)
+
   def getAdjacencyList(input: Seq[(String, String)]): Map[String, Seq[String]] =
-    (input ++ input.map(e => (e._2, e._1))).groupBy(_._1).view.mapValues(_.map(_._2)).toMap
+    (input ++ input.map(e => (e._2, e._1)))
+      .filterNot(e => e._2 == "start")
+      .groupBy(_._1)
+      .view
+      .mapValues(_.map(_._2))
+      .toMap
 
   def getPaths(input: Map[String, Seq[String]]): Seq[Seq[String]] = {
 
-    def helper(currentNode: String, visited: Set[String], path: Seq[String]): Seq[Seq[String]] = {
+    def helper(currentNode: String, visited: Set[String], path: Seq[String]): Seq[Seq[String]] =
       if (visited.contains(currentNode))
         Seq()
       else if (currentNode == "end")
         Seq(path :+ "end")
       else {
-        val currentVisited = if ("[a-z]+".r.matches(currentNode)) visited + currentNode else visited
+        val currentVisited = if (isSmallCave(currentNode)) visited + currentNode else visited
+        input(currentNode).foldLeft(Seq(Seq[String]())) { case (paths, next) =>
+          paths ++ helper(next, currentVisited, path :+ currentNode)
+        }
+      }
+    helper("start", Set.empty, Seq.empty).filterNot(_.isEmpty)
+  }
+
+  def getPathsSmallCaveTwice(input: Map[String, Seq[String]]): Seq[Seq[String]] = {
+
+    def helper(currentNode: String, visited: Map[String, Int], path: Seq[String]): Seq[Seq[String]] = {
+      if ((visited.values.size > 0 && visited.values.max > 2) || visited.values.count(_ >= 2) > 1)
+        Seq()
+      else if (currentNode == "end")
+        Seq(path :+ "end")
+      else {
+        val currentVisited =
+          if (isSmallCave(currentNode)) visited + (currentNode -> (visited(currentNode) + 1)) else visited
         input(currentNode).foldLeft(Seq(Seq[String]())) { case (paths, next) =>
           paths ++ helper(next, currentVisited, path :+ currentNode)
         }
       }
     }
 
-    helper("start", Set.empty, Seq.empty).filterNot(_.isEmpty)
-
+    val ret = helper("start", Map.empty.withDefaultValue(0), Seq.empty).filterNot(_.isEmpty)
+    ret
   }
 
 }
--- advent-of-code-2021-scala/src/test/scala/day12/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day12/SolutionSpec.scala
@@ -13,8 +13,8 @@ object SolutionSpec extends DefaultRunnableSpec {
       } yield assert(getAdjacencyList(input))(
         equalTo(
           Map(
-            "A"     -> List("c", "b", "end", "start"),
-            "b"     -> List("d", "end", "start", "A"),
+            "A"     -> List("c", "b", "end"),
+            "b"     -> List("d", "end", "A"),
             "end"   -> List("A", "b"),
             "c"     -> List("A"),
             "start" -> List("A", "b"),
@@ -36,6 +36,20 @@ object SolutionSpec extends DefaultRunnableSpec {
       for {
         input <- ZFileReader.readLines("day-12-input-william.txt")(parse)
       } yield assert(getPaths(getAdjacencyList(input)).size)(equalTo(3495))
+    },
+    test("getPathsSmallCaveTwice should return the correct number of paths for test input") {
+      for {
+        smallInput  <- ZFileReader.readLines("day12-test-input-small.txt")(parse)
+        mediumInput <- ZFileReader.readLines("day12-test-input-medium.txt")(parse)
+        largeInput  <- ZFileReader.readLines("day12-test-input-large.txt")(parse)
+      } yield assert(getPathsSmallCaveTwice(getAdjacencyList(smallInput)).size)(equalTo(36)) &&
+        assert(getPathsSmallCaveTwice(getAdjacencyList(mediumInput)).size)(equalTo(103)) &&
+        assert(getPathsSmallCaveTwice(getAdjacencyList(largeInput)).size)(equalTo(3509))
+    },
+    test("getPathsSmallCaveTwice should return the correct number of paths for real input") {
+      for {
+        input <- ZFileReader.readLines("day-12-input-william.txt")(parse)
+      } yield assert(getPathsSmallCaveTwice(getAdjacencyList(input)).size)(equalTo(94849))
     }
   )
 }

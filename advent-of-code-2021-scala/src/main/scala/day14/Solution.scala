32817074d8ea34865a7a6f18c5632596cf15008a
William Haw <me@williamhaw.com>
2021-12-14 23:31:06 +0800

    Add day 14 part 2 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day14/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day14/Solution.scala
@@ -26,13 +26,42 @@ object Solution {
         performStep(input, rules)
       else
         helper(n - 1, performStep(input, rules))
-
     helper(n, polymer)
   }
 
   def getScore(polymer: String): Int = {
     val counts = polymer.groupBy(identity).view.mapValues(_.size).values
     counts.max - counts.min
+  }
+
+  def processInput(polymer: String): Map[String, Long] =
+    polymer.sliding(2).toSeq.groupBy(identity).view.mapValues(_.size.toLong).toMap
+
+  def performStepOptimized(state: Map[String, Long], rules: Map[String, String]): Map[String, Long] =
+    state.toSeq
+      .flatMap { case (pair, count) =>
+        rules.get(pair) match {
+          case Some(value) => Seq(s"${pair(0)}$value" -> count, s"$value${pair(1)}" -> count)
+          case None        => Seq(pair -> count)
+        }
+      }
+      .groupMapReduce(_._1)(_._2)(_ + _)
+
+  def performNStepsOptimized(n: Int, state: Map[String, Long], rules: Map[String, String]): Map[String, Long] = {
+    @tailrec
+    def helper(n: Int, input: Map[String, Long]): Map[String, Long] =
+      if (n == 1)
+        performStepOptimized(input, rules)
+      else
+        helper(n - 1, performStepOptimized(input, rules))
+    helper(n, state)
+  }
+
+  def getScoreOptimized(state: Map[String, Long], template: String) = {
+    val charCounts = (template.head -> 1L :: template.last -> 1L :: state.toList.flatMap { case (pair, count) =>
+      Seq(pair(0) -> count, pair(1) -> count)
+    }).groupMapReduce(_._1)(_._2)(_ + _).values.map(_ / 2)
+    charCounts.max - charCounts.min
   }
 
 }
--- advent-of-code-2021-scala/src/test/scala/day14/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day14/SolutionSpec.scala
@@ -33,6 +33,38 @@ object SolutionSpec extends DefaultRunnableSpec {
         template <- ZFileReader.readLines("day-14-input-template-william.txt")(identity).head
         rules    <- ZFileReader.readLines("day-14-input-rules-william.txt")(parseRules)
       } yield assert(getScore(performNSteps(10, template, rules.toMap)))(equalTo(2345))
+    },
+    test("performStepOptimized should return result after one step for test input") {
+      for {
+        template <- ZFileReader.readLines("day14-test-template.txt")(identity).head
+        rules    <- ZFileReader.readLines("day14-test-rules.txt")(parseRules)
+      } yield assert(performStepOptimized(processInput(template), rules.toMap))(
+        equalTo(Map("NC" -> 1L, "CN" -> 1L, "NB" -> 1L, "BC" -> 1L, "CH" -> 1L, "HB" -> 1L))
+      )
+    },
+    test("getScoreOptimized should return score after 10 steps for test input") {
+      for {
+        template <- ZFileReader.readLines("day14-test-template.txt")(identity).head
+        rules    <- ZFileReader.readLines("day14-test-rules.txt")(parseRules)
+      } yield assert(getScoreOptimized(performNStepsOptimized(10, processInput(template), rules.toMap), template))(
+        equalTo(1588L)
+      )
+    },
+    test("getScoreOptimized should return score after 10 steps for real input") {
+      for {
+        template <- ZFileReader.readLines("day-14-input-template-william.txt")(identity).head
+        rules    <- ZFileReader.readLines("day-14-input-rules-william.txt")(parseRules)
+      } yield assert(getScoreOptimized(performNStepsOptimized(10, processInput(template), rules.toMap), template))(
+        equalTo(2345L)
+      )
+    },
+    test("getScoreOptimized should return score after 40 steps for real input") {
+      for {
+        template <- ZFileReader.readLines("day-14-input-template-william.txt")(identity).head
+        rules    <- ZFileReader.readLines("day-14-input-rules-william.txt")(parseRules)
+      } yield assert(getScoreOptimized(performNStepsOptimized(40, processInput(template), rules.toMap), template))(
+        equalTo(2432786807053L)
+      )
     }
   )
 }

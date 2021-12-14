eab7132ce4bf423b94c941fb45a61467a70abcd4
William Haw <me@williamhaw.com>
2021-12-14 14:44:00 +0800

    Add day 14 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day14/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day14/Solution.scala
@@ -1,5 +1,38 @@
 package day14
 
+import scala.annotation.tailrec
+
 object Solution {
+
+  def parseRules(line: String): (String, String) = line match {
+    case s"$pair -> $insertChar" => pair -> insertChar
+  }
+
+  def performStep(polymer: String, rules: Map[String, String]): String =
+    polymer
+      .sliding(2)
+      .map { currentPair =>
+        rules.get(currentPair) match {
+          case Some(value) => s"${currentPair(0)}$value${currentPair(1)}"
+          case None        => currentPair
+        }
+      }
+      .foldLeft("") { (accum, current) => if (accum.isEmpty()) current else accum + current.tail }
+
+  def performNSteps(n: Int, polymer: String, rules: Map[String, String]): String = {
+    @tailrec
+    def helper(n: Int, input: String): String =
+      if (n == 1)
+        performStep(input, rules)
+      else
+        helper(n - 1, performStep(input, rules))
+
+    helper(n, polymer)
+  }
+
+  def getScore(polymer: String): Int = {
+    val counts = polymer.groupBy(identity).view.mapValues(_.size).values
+    counts.max - counts.min
+  }
 
 }
--- advent-of-code-2021-scala/src/test/scala/day14/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day14/SolutionSpec.scala
@@ -7,8 +7,32 @@ import day14.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day14.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("performStep should return result after one step for test input") {
+      for {
+        template <- ZFileReader.readLines("day14-test-template.txt")(identity).head
+        rules    <- ZFileReader.readLines("day14-test-rules.txt")(parseRules)
+      } yield assert(performStep(template, rules.toMap))(equalTo("NCNBCHB"))
+    },
+    test("performNSteps should return result after n steps for test input") {
+      for {
+        template <- ZFileReader.readLines("day14-test-template.txt")(identity).head
+        rules    <- ZFileReader.readLines("day14-test-rules.txt")(parseRules)
+      } yield assert(performNSteps(1, template, rules.toMap))(equalTo("NCNBCHB")) &&
+        assert(performNSteps(2, template, rules.toMap))(equalTo("NBCCNBBBCBHCB")) &&
+        assert(performNSteps(3, template, rules.toMap))(equalTo("NBBBCNCCNBBNBNBBCHBHHBCHB")) &&
+        assert(performNSteps(4, template, rules.toMap))(equalTo("NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"))
+    },
+    test("getScore should return score after 10 steps for test input") {
+      for {
+        template <- ZFileReader.readLines("day14-test-template.txt")(identity).head
+        rules    <- ZFileReader.readLines("day14-test-rules.txt")(parseRules)
+      } yield assert(getScore(performNSteps(10, template, rules.toMap)))(equalTo(1588))
+    },
+    test("getScore should return score after 10 steps for real input") {
+      for {
+        template <- ZFileReader.readLines("day-14-input-template-william.txt")(identity).head
+        rules    <- ZFileReader.readLines("day-14-input-rules-william.txt")(parseRules)
+      } yield assert(getScore(performNSteps(10, template, rules.toMap)))(equalTo(2345))
     }
   )
 }

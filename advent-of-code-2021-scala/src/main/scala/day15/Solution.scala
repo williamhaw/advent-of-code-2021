1a2a95d2e6a7670e1db4ff25db55fb97e0f021a7
William Haw <me@williamhaw.com>
2021-12-15 23:13:32 +0800

    Add day 15 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day15/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day15/Solution.scala
@@ -1,5 +1,38 @@
 package day15
 
+import scala.annotation.tailrec
+
 object Solution {
-  
+
+  def parse(line: String): Seq[Int] = line.map(_.toInt - 48)
+
+  def getLowestRisk(input: Seq[Seq[Int]]): Int = {
+    val start = (0, 0)
+    val end   = (input.size - 1, input(0).size - 1)
+
+    val directions = Seq((-1, 0), (0, -1), (1, 0), (0, 1))
+
+    def helper(risks: Map[(Int, Int), Int], queue: Seq[(Int, Int)]): Map[(Int, Int), Int] =
+      if (queue.isEmpty)
+        risks
+      else {
+        val current                  = queue.head
+        val (currentRow, currentCol) = current
+
+        val neighbours = directions
+          .map { case (dr, dc) => (currentRow + dr, currentCol + dc) }
+          .filterNot { case (r, c) => r < 0 || c < 0 || r > end._1 || c > end._2 }
+
+        val newRisk: Map[(Int, Int), Int] = neighbours.collect {
+          case next @ (nextRow, nextCol)
+              if !risks.contains(next) || risks(current) + input(nextRow)(nextCol) < risks(next) =>
+            next -> (risks(current) + input(nextRow)(nextCol))
+        }.toMap
+
+        helper(risks ++ newRisk, queue.tail :++ newRisk.keySet)
+      }
+
+    helper(Map(start -> 0), Seq(start))(end)
+  }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day15/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day15/SolutionSpec.scala
@@ -7,8 +7,15 @@ import day15.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day15.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("getLowestRisk should return lowest total risk for test input") {
+      for {
+        rawInput <- ZFileReader.readLines("day15-test-input.txt")(parse)
+      } yield assert(getLowestRisk(rawInput))(equalTo(40))
+    },
+    test("getLowestRisk should return lowest total risk for real input") {
+      for {
+        rawInput <- ZFileReader.readLines("day-15-input-william.txt")(parse)
+      } yield assert(getLowestRisk(rawInput))(equalTo(441))
     }
   )
 }

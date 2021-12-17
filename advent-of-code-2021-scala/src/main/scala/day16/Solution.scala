cdddf13b88bd37c9de3efa7fb0e5e9c6e44f884d
William Haw <me@williamhaw.com>
2021-12-17 13:53:17 +0800

    Add day 16 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day16/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day16/Solution.scala
@@ -1,5 +1,108 @@
 package day16
 
 object Solution {
-    
+
+  sealed trait Packet {
+    def packetTypeId: Int
+    def bitLength: Int
+  }
+
+  case class Literal(version: Int, value: Long, bitLength: Int) extends Packet {
+    val packetTypeId = 4
+  }
+
+  case class Operator(
+      version: Int,
+      packetTypeId: Int,
+      lengthType: LengthType,
+      bitLength: Int,
+      subPackets: Seq[Packet]
+  ) extends Packet
+
+  sealed trait LengthType
+  case class Zero(subPacketsBitLength: Int) extends LengthType
+  case class One(numSubPackets: Int)        extends LengthType
+
+  object Packet {
+    def apply(transmission: String): Packet = {
+      //   println(transmission.grouped(4).toSeq)
+      val version    = Integer.parseInt(transmission.take(3), 2)
+      val packetType = Integer.parseInt(transmission.slice(3, 6), 2)
+      if (packetType == 4) {
+        val digitBits = transmission.slice(6, transmission.size)
+        var stop      = digitBits.head
+        var cursor    = 0
+        while (stop == '1') {
+          cursor += 5
+          stop = digitBits(cursor)
+        }
+        cursor += 5
+        Literal(version, 0L, cursor + 6)
+      } else {
+        val lengthTypeId = Integer.parseInt(transmission.slice(6, 7), 2)
+        if (lengthTypeId == 0) {
+          val length = Integer.parseInt(transmission.slice(7, 22), 2)
+          Operator(
+            version,
+            packetType,
+            Zero(length),
+            3 + 3 + 1 + 15 + length,
+            readAllPacketsWithBitLength(transmission.drop(22), length)
+          )
+        } else {
+          val numSubPackets = Integer.parseInt(transmission.slice(7, 18), 2)
+          val subPackets    = readAllPacketsWithNumPackets(transmission.drop(18), numSubPackets)
+          Operator(
+            version,
+            packetType,
+            One(numSubPackets),
+            3 + 3 + 1 + 11 + subPackets.map(_.bitLength).sum,
+            subPackets
+          )
+        }
+
+      }
+    }
+
+    def readAllPacketsWithBitLength(input: String, bitLength: Int): Seq[Packet] = {
+      var ret: Seq[Packet] = Seq.empty
+      var cursor           = 0
+      while (cursor < bitLength) {
+        val current = Packet(input.drop(cursor))
+        ret = ret :+ current
+        cursor += current.bitLength
+      }
+      ret
+    }
+
+    def readAllPacketsWithNumPackets(input: String, numPackets: Int): Seq[Packet] = {
+      var cursor = 0
+      for {
+        _ <- 0 until numPackets
+      } yield {
+        val p = Packet(input.drop(cursor))
+        cursor += p.bitLength
+        p
+      }
+    }
+  }
+
+  def parseTransmission(line: String): String = {
+    val sb = new StringBuilder()
+    line.strip().foreach { c =>
+      val b = Integer.parseInt(c.toString(), 16).toBinaryString.reverse.padTo(4, '0').reverse
+      sb.append(b)
+    }
+    sb.toString()
+  }
+
+  def getSumOfVersions(transmission: String): Int = {
+    val packet = Packet.apply(transmission)
+    def helper(p: Packet): Int = p match {
+      case Literal(version, _, _)                 => version
+      case Operator(version, _, _, _, subPackets) => version + subPackets.map(helper).sum
+    }
+    helper(packet)
+  }
+
 }
--- advent-of-code-2021-scala/src/test/scala/day16/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day16/SolutionSpec.scala
@@ -7,8 +7,51 @@ import day16.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day16.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("Packet.apply should return literal value for literal value transmission") {
+      assert(Packet(parseTransmission("D2FE28")))(equalTo(Literal(6, 0L, 21)))
+    },
+    test("Packet.apply should return value for operator transmission") {
+      assert(Packet(parseTransmission("38006F45291200")))(
+        equalTo(
+          Operator(
+            version = 1,
+            packetTypeId = 6,
+            lengthType = Zero(27),
+            bitLength = 49,
+            Seq(
+              Literal(6, 0L, 11),
+              Literal(2, 0L, 16)
+            )
+          )
+        )
+      ) &&
+      assert(Packet(parseTransmission("EE00D40C823060")))(
+        equalTo(
+          Operator(
+            version = 7,
+            packetTypeId = 3,
+            lengthType = One(3),
+            bitLength = 51,
+            Seq(
+              Literal(2, 0L, 11),
+              Literal(4, 0L, 11),
+              Literal(1, 0L, 11)
+            )
+          )
+        )
+      )
+    },
+    test("getSumOfVersions should get sum of versions for test input") {
+      assert(getSumOfVersions(parseTransmission("38006F45291200")))(equalTo(9)) &&
+      assert(getSumOfVersions(parseTransmission("8A004A801A8002F478")))(equalTo(16)) &&
+      assert(getSumOfVersions(parseTransmission("620080001611562C8802118E34")))(equalTo(12)) &&
+      assert(getSumOfVersions(parseTransmission("C0015000016115A2E0802F182340")))(equalTo(23)) &&
+      assert(getSumOfVersions(parseTransmission("A0016C880162017C3686B18A3D4780")))(equalTo(31))
+    },
+    test("getSumOfVersions should get sum of versions for real input") {
+      for {
+        bin <- ZFileReader.readLines("day-16-input-william.txt")(parseTransmission).head
+      } yield assert(getSumOfVersions(bin))(equalTo(955))
     }
   )
 }

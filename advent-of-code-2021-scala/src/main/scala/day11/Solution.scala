d04f7897ce1032ea50448ca216f195e857f19b76
William Haw <me@williamhaw.com>
2021-12-18 12:15:27 +0800

    Add day 11 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day11/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day11/Solution.scala
@@ -1,7 +1,61 @@
 package day11
 
+import scala.annotation.tailrec
+
 object Solution {
 
-  def parse(line: String): Seq[Int] = line.strip().map(_.toInt - 48)
+  def parse(line: String): Seq[Int] = line.strip().map(_.asDigit)
+
+  def toSeqSeq(grid: Array[Array[Int]]): Seq[Seq[Int]] = grid.map(_.toSeq).toSeq
+
+  val directions = Seq((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+
+  def getNeighbours(point: (Int, Int), grid: Seq[Seq[Int]]): Set[(Int, Int)] =
+    directions
+      .map { case (dx, dy) => (point._1 + dx, point._2 + dy) }
+      .filter { case (r, c) => grid.indices.contains(r) && grid(0).indices.contains(c) }
+      .toSet
+
+  def incrementLevel(grid: Seq[Seq[Int]]): Seq[Seq[Int]] =
+    grid.map(row => row.map(ele => ele + 1))
+
+  @tailrec
+  def getFlashed(grid: Seq[Seq[Int]], flashed: Set[(Int, Int)]): Set[(Int, Int)] = {
+    val next = flashed ++
+      flashed
+        .flatMap(p => getNeighbours(p, grid))
+        .filter(candidate =>
+          grid(candidate._1)(candidate._2) + flashed.intersect(getNeighbours(candidate, grid)).size > 9
+        )
+    if (next == flashed) flashed else getFlashed(grid, next)
+  }
+
+  def step(grid: Seq[Seq[Int]]): Seq[Seq[Int]] = {
+    val incr = incrementLevel(grid)
+    val initialFlash = (for { r <- grid.indices; c <- grid(0).indices } yield (r, c)).filter { case (r, c) =>
+      incr(r)(c) > 9
+    }.toSet
+    val flashed = getFlashed(incr, initialFlash)
+    incr.zipWithIndex.map { case (row, r) =>
+      row.zipWithIndex.map { case (ele, c) =>
+        if (flashed.contains((r, c))) 0 else ele + getNeighbours((r, c), incr).count(flashed.contains)
+      }
+    }
+  }
+
+  @tailrec
+  def stepN(n: Int, grid: Seq[Seq[Int]]): Seq[Seq[Int]] =
+    if (n > 0)
+      stepN(n - 1, step(grid))
+    else
+      grid
+
+  @tailrec
+  def stepNTotalFlashes(n: Int, grid: Seq[Seq[Int]], count: Int): Int =
+    if (n > 0) {
+      val output = step(grid)
+      stepNTotalFlashes(n - 1, output, count + output.map(_.count(_ == 0)).sum)
+    } else
+      count
 
 }
--- advent-of-code-2021-scala/src/test/scala/day11/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day11/SolutionSpec.scala
@@ -3,12 +3,95 @@ package day11
 import zio.test._
 import Assertion._
 import common.ZFileReader
-import day10.Solution._
+import day11.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day11.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("step should return grid after multiple flashes") {
+      val input = Seq(
+        Seq(1, 1, 1, 1, 1),
+        Seq(1, 9, 9, 9, 1),
+        Seq(1, 9, 1, 9, 1),
+        Seq(1, 9, 9, 9, 1),
+        Seq(1, 1, 1, 1, 1)
+      )
+      assert(step(input))(
+        equalTo(
+          Seq(
+            Seq(3, 4, 5, 4, 3),
+            Seq(4, 0, 0, 0, 4),
+            Seq(5, 0, 0, 0, 5),
+            Seq(4, 0, 0, 0, 4),
+            Seq(3, 4, 5, 4, 3)
+          )
+        )
+      )
+    },
+    test("step should return grid after one step from test input") {
+      for {
+        input <- ZFileReader.readLines("day11-test-input.txt")(parse)
+      } yield assert(step(input))(
+        equalTo(
+          Seq(
+            Seq(6, 5, 9, 4, 2, 5, 4, 3, 3, 4),
+            Seq(3, 8, 5, 6, 9, 6, 5, 8, 2, 2),
+            Seq(6, 3, 7, 5, 6, 6, 7, 2, 8, 4),
+            Seq(7, 2, 5, 2, 4, 4, 7, 2, 5, 7),
+            Seq(7, 4, 6, 8, 4, 9, 6, 5, 8, 9),
+            Seq(5, 2, 7, 8, 6, 3, 5, 7, 5, 6),
+            Seq(3, 2, 8, 7, 9, 5, 2, 8, 3, 2),
+            Seq(7, 9, 9, 3, 9, 9, 2, 2, 4, 5),
+            Seq(5, 9, 5, 7, 9, 5, 9, 6, 6, 5),
+            Seq(6, 3, 9, 4, 8, 6, 2, 6, 3, 7)
+          )
+        )
+      )
+    },
+    test("stepN should return grid after n steps from test input") {
+      for {
+        input <- ZFileReader.readLines("day11-test-input.txt")(parse)
+      } yield assert(stepN(2, input))(
+        equalTo(
+          Seq(
+            Seq(8, 8, 0, 7, 4, 7, 6, 5, 5, 5),
+            Seq(5, 0, 8, 9, 0, 8, 7, 0, 5, 4),
+            Seq(8, 5, 9, 7, 8, 8, 9, 6, 0, 8),
+            Seq(8, 4, 8, 5, 7, 6, 9, 6, 0, 0),
+            Seq(8, 7, 0, 0, 9, 0, 8, 8, 0, 0),
+            Seq(6, 6, 0, 0, 0, 8, 8, 9, 8, 9),
+            Seq(6, 8, 0, 0, 0, 0, 5, 9, 4, 3),
+            Seq(0, 0, 0, 0, 0, 0, 7, 4, 5, 6),
+            Seq(9, 0, 0, 0, 0, 0, 0, 8, 7, 6),
+            Seq(8, 7, 0, 0, 0, 0, 6, 8, 4, 8)
+          )
+        )
+      ) && assert(stepN(10, input))(
+        equalTo(
+          Seq(
+            Seq(0, 4, 8, 1, 1, 1, 2, 9, 7, 6),
+            Seq(0, 0, 3, 1, 1, 1, 2, 0, 0, 9),
+            Seq(0, 0, 4, 1, 1, 1, 2, 5, 0, 4),
+            Seq(0, 0, 8, 1, 1, 1, 1, 4, 0, 6),
+            Seq(0, 0, 9, 9, 1, 1, 1, 3, 0, 6),
+            Seq(0, 0, 9, 3, 5, 1, 1, 2, 3, 3),
+            Seq(0, 4, 4, 2, 3, 6, 1, 1, 3, 0),
+            Seq(5, 5, 3, 2, 2, 5, 2, 3, 5, 0),
+            Seq(0, 5, 3, 2, 2, 5, 0, 6, 0, 0),
+            Seq(0, 0, 3, 2, 2, 4, 0, 0, 0, 0)
+          )
+        )
+      )
+    },
+    test("stepNTotalFlashes should return total number of flashes after n steps from test input") {
+      for {
+        input <- ZFileReader.readLines("day11-test-input.txt")(parse)
+      } yield assert(stepNTotalFlashes(10, input, 0))(equalTo(204)) &&
+        assert(stepNTotalFlashes(100, input, 0))(equalTo(1656))
+    },
+    test("stepNTotalFlashes should return total number of flashes after n steps from real input") {
+      for {
+        input <- ZFileReader.readLines("day-11-input-william.txt")(parse)
+      } yield assert(stepNTotalFlashes(100, input, 0))(equalTo(1591))
     }
   )
 

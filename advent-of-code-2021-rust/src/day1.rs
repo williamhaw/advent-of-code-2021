06f65470eceb12aeb68353dd1e58c4dd2a0b7699
William Haw <me@williamhaw.com>
2021-12-19 14:17:12 +0800

    [Rust] Fix tests and add day 1 part 2 solution

--- advent-of-code-2021-rust/src/day1.rs
+++ advent-of-code-2021-rust/src/day1.rs
@@ -6,14 +6,18 @@ fn parse(line: String) -> i32 {
     line.parse::<i32>().unwrap()
 }
 
-fn count_increase(input: &Vec<i32>) -> i32 {
+fn count_increase(input: Vec<i32>) -> i32 {
     let mut count = 0;
-    for v in [&input].windows(2) {
+    for v in input.windows(2) {
         if (v[1] > v[0]) {
             count = count + 1
         }
     }
     return count;
+}
+
+fn count_increase_three_window(input: Vec<i32>) -> i32 {
+    return count_increase(input.windows(3).map(|v| v.iter().sum()).collect::<Vec<i32>>());
 }
 
 #[cfg(test)]
@@ -22,7 +26,13 @@ mod test {
     use crate::common::read_lines;
     #[test]
     fn count_increase_gets_correct_count_for_real_input() {
-        let input = read_lines(Path::new("resources/day-1-input-william.txt"), parse);
-        assert_eq!(true, true)
+        let input = read_lines(Path::new("resources/day-1-input-william.txt"), parse).unwrap();
+        assert_eq!(count_increase(input), 1195)
+    }
+
+    #[test]
+    fn count_increase_three_window_gets_correct_count_for_real_input() {
+        let input = read_lines(Path::new("resources/day-1-input-william.txt"), parse).unwrap();
+        assert_eq!(count_increase_three_window(input), 1235)
     }
 }

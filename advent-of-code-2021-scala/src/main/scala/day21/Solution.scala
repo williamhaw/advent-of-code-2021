ddb36dcaa7d5bbffedc88b1ccf940248967508f9
William Haw <me@williamhaw.com>
2021-12-21 23:32:56 +0800

    [Scala] Add day 21 part 1 solution and tests

--- advent-of-code-2021-scala/src/main/scala/day21/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day21/Solution.scala
@@ -1,5 +1,50 @@
 package day21
 
 object Solution {
-  
+
+  def parse(line: String): Int = line match {
+    case s"Player $_ starting position: $p" => p.toInt
+  }
+
+  case class Turn(position: Int, lastDiceThrow: Int, score: Int)
+
+  def turn(previous: Turn): Turn = {
+
+    val firstRoll  = roll(previous.lastDiceThrow)
+    val secondRoll = roll(firstRoll)
+    val thirdRoll  = roll(secondRoll)
+
+    val newPosition = move(previous.position, firstRoll + secondRoll + thirdRoll)
+    val newScore    = previous.score + newPosition
+    // println("turn", firstRoll, secondRoll, thirdRoll, newPosition)
+
+    Turn(newPosition, thirdRoll, newScore)
+  }
+
+  def roll(prev: Int): Int = if (prev % 100 == 0) 1 else prev + 1
+
+  def move(prev: Int, move: Int): Int = if ((prev + move) % 10 == 0) 10 else (prev + move) % 10
+
+  def game(positions: Seq[Int]): Int = {
+    val playerOnePosition = positions(0)
+    val playerTwoPosition = positions(1)
+
+    def helper(isPlayerOne: Boolean, prevPlayerOneTurn: Turn, prevPlayerTwoTurn: Turn, numDiceRolls: Int): Int = {
+    //   println(if (isPlayerOne) "p1" else "p2", prevPlayerOneTurn, prevPlayerTwoTurn, numDiceRolls)
+      if (prevPlayerOneTurn.score >= 1000 || prevPlayerTwoTurn.score >= 1000) {
+        if (prevPlayerOneTurn.score < prevPlayerTwoTurn.score)
+          prevPlayerOneTurn.score * numDiceRolls
+        else
+          prevPlayerTwoTurn.score * numDiceRolls
+      } else if (isPlayerOne) {
+        val newTurn = turn(prevPlayerOneTurn.copy(lastDiceThrow = prevPlayerTwoTurn.lastDiceThrow))
+        helper(false, newTurn, prevPlayerTwoTurn, numDiceRolls + 3)
+      } else {
+        val newTurn = turn(prevPlayerTwoTurn.copy(lastDiceThrow = prevPlayerOneTurn.lastDiceThrow))
+        helper(true, prevPlayerOneTurn, newTurn, numDiceRolls + 3)
+      }
+    }
+
+    helper(false, turn(Turn(playerOnePosition, 100, 0)), Turn(playerTwoPosition, 3, 0), 3)
+  }
 }
--- advent-of-code-2021-scala/src/test/scala/day21/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day21/SolutionSpec.scala
@@ -7,8 +7,32 @@ import day21.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day21.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("roll should return correct result from boundary") {
+      assert(roll(100))(equalTo(1)) &&
+      assert(roll(99))(equalTo(100))
+    },
+    test("move should return correct result from boundary") {
+      assert(move(10, 1))(equalTo(1)) &&
+      assert(move(9, 2))(equalTo(1)) &&
+      assert(move(9, 1))(equalTo(10))
+    },
+    test("turn should return correct state from test input") {
+      assert(turn(Turn(4, 100, 0)))(equalTo(Turn(10, 3, 10))) &&
+      assert(turn(Turn(8, 3, 0)))(equalTo(Turn(3, 6, 3))) &&
+      assert(turn(Turn(10, 6, 10)))(equalTo(Turn(4, 9, 14))) &&
+      assert(turn(Turn(3, 9, 3)))(equalTo(Turn(6, 12, 9))) &&
+      assert(turn(Turn(4, 12, 14)))(equalTo(Turn(6, 15, 20))) &&
+      assert(turn(Turn(6, 15, 9)))(equalTo(Turn(7, 18, 16))) &&
+      assert(turn(Turn(6, 18, 20)))(equalTo(Turn(6, 21, 26))) &&
+      assert(turn(Turn(7, 21, 16)))(equalTo(Turn(6, 24, 22)))
+    },
+    test("game should return correct score for test input") {
+      assert(game(Seq(4, 8)))(equalTo(739785))
+    },
+    test("game should return correct score for real input") {
+      for {
+        positions <- ZFileReader.readLines("day-21-input-william.txt")(parse)
+      } yield assert(game(positions))(equalTo(920079))
     }
   )
 }

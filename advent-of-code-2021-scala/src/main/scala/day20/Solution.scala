3c483c8cfbf3700745e285fa35a2edce5d7092b4
William Haw <me@williamhaw.com>
2021-12-21 12:53:52 +0800

    Add solution and tests for day 20

--- advent-of-code-2021-scala/src/main/scala/day20/Solution.scala
+++ advent-of-code-2021-scala/src/main/scala/day20/Solution.scala
@@ -1,5 +1,63 @@
 package day20
 
+import scala.annotation.tailrec
+
 object Solution {
-  
+
+  def parse(line: String): Seq[Char] = line.strip().toCharArray()
+
+  def processFloorSet(input: Seq[Seq[Char]]): Set[(Int, Int)] =
+    (for {
+      r <- input.indices
+      c <- input(0).indices
+      if input(r)(c) == '#'
+    } yield (r, c)).toSet
+
+  def parseAlgorithm(line: String): Seq[Boolean] = line.strip().map {
+    case '#' => true
+    case '.' => false
+  }
+
+  def getIndex(
+      floor: Set[(Int, Int)],
+      point: (Int, Int),
+      topLeft: (Int, Int),
+      bottomRight: (Int, Int),
+      flipBit: Boolean
+  ): Int = {
+    val (row, col)       = point
+    val (minRow, minCol) = topLeft
+    val (maxRow, maxCol) = bottomRight
+    var index            = 0
+    for {
+      r <- (row - 1) to (row + 1)
+      c <- (col - 1) to (col + 1)
+    } yield {
+      index <<= 1
+      index = index | (if (floor.contains((r, c))) 1 else 0)
+      index = index | (if (flipBit && (r < minRow || r > maxRow || c < minCol || c > maxCol)) 1 else 0)
+    }
+    index
+  }
+
+  def step(floor: Set[(Int, Int)], algorithm: Seq[Boolean], flipBit: Boolean): Set[(Int, Int)] = {
+    val (minRow, minColumn) = (floor.map(_._1).min, floor.map(_._2).min)
+    val (maxRow, maxColumn) = (floor.map(_._1).max, floor.map(_._2).max)
+
+    (
+      for {
+        r <- (minRow - 1) to (maxRow + 1)
+        c <- (minColumn - 1) to (maxColumn + 1)
+        if algorithm(getIndex(floor, (r, c), (minRow, minColumn), (maxRow, maxColumn), flipBit))
+      } yield (r, c)
+    ).toSet
+  }
+
+  @tailrec
+  def takeNSteps(n: Int, floor: Set[(Int, Int)], algorithm: Seq[Boolean]): Set[(Int, Int)] =
+    if (n == 0)
+      floor
+    else {
+      takeNSteps(n - 1, step(floor, algorithm, algorithm(0) && (n % 2 == 1)), algorithm)
+    }
 }
--- advent-of-code-2021-scala/src/test/scala/day20/SolutionSpec.scala
+++ advent-of-code-2021-scala/src/test/scala/day20/SolutionSpec.scala
@@ -7,8 +7,24 @@ import day20.Solution._
 
 object SolutionSpec extends DefaultRunnableSpec {
   def spec = suite("day20.SolutionSpec")(
-    test("") {
-      assertTrue(true)
+    test("getIndex should return correct value from test input") {
+      val input = Set((1, 0), (2, 1))
+      assert(getIndex(input, (1, 1), (1, 0), (2, 1), false))(equalTo(34))
+    },
+    test("takeNSteps should return the correct number of lit pixels from test input") {
+      for {
+        algorithm <- ZFileReader.readLines("day20-test-input-algorithm.txt")(parseAlgorithm).head
+        rawFloor  <- ZFileReader.readLines("day20-test-input-image.txt")(parse)
+      } yield assert(takeNSteps(1, processFloorSet(rawFloor), algorithm).size)(equalTo(24)) &&
+        assert(takeNSteps(2, processFloorSet(rawFloor), algorithm).size)(equalTo(35)) &&
+        assert(takeNSteps(50, processFloorSet(rawFloor), algorithm).size)(equalTo(3351))
+    },
+    test("takeNSteps should return the correct number of lit pixels from real input") {
+      for {
+        algorithm <- ZFileReader.readLines("day-20-input-algorithm-william.txt")(parseAlgorithm).head
+        rawFloor  <- ZFileReader.readLines("day-20-input-image-william.txt")(parse)
+      } yield assert(takeNSteps(2, processFloorSet(rawFloor), algorithm).size)(equalTo(5647)) &&
+        assert(takeNSteps(50, processFloorSet(rawFloor), algorithm).size)(equalTo(15653))
     }
   )
 }
